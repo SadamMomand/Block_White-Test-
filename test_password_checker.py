8de165a322fa41d64492e06beb00d74cda275d20
SadamMomand <sadam.moahmand.dara@bbs1-lueneburg.de>
2026-09-11 11:38:31 +0200

    test3

--- test_password_checker.py
+++ test_password_checker.py
@@ -1,13 +1,13 @@
-from password_checker import in_valid_password 
+from password_checker import is_valid_password 
 
 def test_valid_password():
-    assert in_valid_password('abc12345') == True
+    assert is_valid_password('abc12345') == True
 
 def test_password_too_short():
-    assert in_valid_password('abc123') == False
+    assert is_valid_password('abc123') == False
 
 def test_password_without_number():
-    assert in_valid_password('abcdefgh') == False
+    assert is_valid_password('abcdefgh') == False
 
 
 
